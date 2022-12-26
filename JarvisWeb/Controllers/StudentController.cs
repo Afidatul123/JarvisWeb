2545b60ca02cbd5107407ba478a3488aa06298b8
sitompuledward180801@hotmail.com <sitompuledward180801@hotmail.com>
2022-12-26 21:58:25 +0700

    add static data

---
+++ JarvisWeb/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using JarvisWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JarvisWeb.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            var student = new Student()
+            {
+                Id = 1,
+                Name = "Afida",
+                Addres = "Salatiga"
+            };
+
+            return View(student);
+        }
+    }
+}
---
+++ JarvisWeb/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace JarvisWeb.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Addres { get; set; }
+
+
+    }
+}
